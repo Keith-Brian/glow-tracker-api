47f740481a65361d92270a6c1ac077c372e17f8f
keith-brian <chiefdeveloper254@gmail.com>
2025-12-25 17:33:58 +0300

    set up africastalking api

---
+++ configure/africastalking.js
@@ -0,0 +1,15 @@
+// here is a configuration file to add the Africa's Talking API setup
+
+require('dotenv').config();
+const Africastalking = require('africastalking');
+
+const apiCredentials = {
+    apiKey:process.env.AT_KEY,
+    username:process.env.AT_USERNAME
+}
+
+const africastalking = Africastalking(apiCredentials);
+
+const sms = africastalking.SMS;
+
+module.exports = sms;
