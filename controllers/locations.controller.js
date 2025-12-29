5d2613309717a88d9e2880547554bd6f9ada46f1
keith-brian <chiefdeveloper254@gmail.com>
2025-12-29 18:06:53 +0300

    update location based on auth

--- controllers/locations.controller.js
+++ controllers/locations.controller.js
@@ -1,44 +1,54 @@
-const Location = require('../models/locations.model.js');
-const Device = require('../models/devices.model.js');
-
+const Location = require("../models/locations.model.js");
+const Device = require("../models/devices.model.js");
 
 //getting all live locations from the dataBase
 
 const getAllLiveLocations = async (req, res) => {
-    try{
-        const locations = await Location.find({});
-        res.status(200).json(locations);
-    }
-    catch(error){
-        res.status(500).json({message: 'Server Error', error: error.message});
-    }
-}
-
-
-
+  try {
+    const locations = await Location.find({});
+    res.status(200).json(locations);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
 
 //adding a live location to the dataBase;
 const addLiveLocation = async (req, res) => {
-    try{
-        const { deviceId } = req.params;
-        const device = await Device.findOne({deviceId});
+  try {
+    const deviceIdAuth = req.device.deviceId;
+    const { deviceId,location, speed, gpsFix, battery, signal } = req.body;
 
-        if(!device){
-            return res.status(404).json({message: 'Device not found'});
-        }
+    console.log("Device ID from auth middleware:", deviceIdAuth);
+    console.log("Device ID from request body:", deviceId);
 
-        const liveLocation = await Location.create(req.body);
-        res.status(200).json(liveLocation);
-    
+    // make sure the deviceId in the token matches the deviceId in the request body
+    if (deviceIdAuth !== deviceId) {
+      return res.status(403).json({ message: "Device ID mismatch" });
     }
-    catch(error){
-        res.status(500).json({message: 'Server Error', error: error.message});
-    }
-}
 
+    console.log("Device ID from auth middleware:", deviceId);
+    const device = await Device.findOne({ deviceId: deviceId });
+
+    if (!device) {
+      return res.status(404).json({ message: "Device not found" });
+    }
+
+    const liveLocation = await Location.create({
+        deviceId: deviceId,
+        userId: device.userId,
+        location: location,
+        speed: speed,
+        gpsFix: gpsFix,
+        battery: battery,
+        signal: signal
+    });
+    res.status(200).json(liveLocation);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
 
 module.exports = {
-    getAllLiveLocations,
-    addLiveLocation
-}; 
-
+  getAllLiveLocations,
+  addLiveLocation,
+};
--- routes/locations.route.js
+++ routes/locations.route.js
@@ -5,13 +5,16 @@ const router = express.Router();
 const Location = require('../models/locations.model.js');
 const { getAllLiveLocations, addLiveLocation } = require('../controllers/locations.controller.js');
 
+// add the device authentication middleware
+const deviceAuthentication = require('../middleware/device-auth.middleware.js');
+
 // Route to get the live-locations for all devices 
 
 // get all live-locations in the db
 router.get('/', getAllLiveLocations);
 
 // add a new live-location to the dB
-router.post('/:deviceId', addLiveLocation);
+router.post('/updateLocation', deviceAuthentication, addLiveLocation);
 
 // TODO: option to delete the old locations after a certain period
 
